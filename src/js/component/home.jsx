2aa4aeb99f0ad0b7ff19aa1ea1a98c9fae4a6326
moniasuka <moniasuka@hotmail.com>
2022-10-03 00:42:35 +0000

    ejercicio traffic light completado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,27 @@
-import React from "react";
+import React ,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+export const Home = () => {
+	//Se agrega el hook (linea 13) para poder cambiar el estado de clickedColor al hacer click 
+	//en el div y poder agregar el estilo "glow" del className.
+	//La funcion onClick (lineas 19, 20 y 21) hace que al detectar un click del usuario, el estado de la variable 
+	//clickedColor cambie a "glow" y esta propiedad se le agregue al ClasName
+	
+	
+	const [clickedColor, setClickedcolor] = useState("");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 ">
+			<div className="stick"></div>
+			<div className="traffic-light">
+				<div onClick={() => setClickedcolor("red")} className={"light red"  + (clickedColor === "red" ? " glow" : "")}></div>
+				<div onClick={() => setClickedcolor("yellow")} className={"light yellow"  + (clickedColor === "yellow" ? " glow" : "")}></div>
+				<div onClick={() => setClickedcolor("green")} className={"light green"  + (clickedColor === "green" ? " glow" : "")}></div>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+
